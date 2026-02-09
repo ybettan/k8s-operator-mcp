b1f04b78710100e18091cf200ac9be7ef283514e
Yoni Bettan <yonibettan@gmail.com>
2026-02-09 12:40:31 +0200

    Updated the `CreateOperatorTemplate` to create an empty dir.
    The code will be generate in a future commit.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- server/main.go
+++ server/main.go
@@ -12,7 +12,14 @@ func main() {
 
 	server := mcp.NewServer(&mcp.Implementation{Name: "k8s-operator-manager", Version: "v1.0.0"}, nil)
 
-	mcp.AddTool(server, &mcp.Tool{Name: "create-operator-template", Description: "creates HW operator template"}, tools.CreateOperatorTemplate)
+	mcp.AddTool(
+		server,
+		&mcp.Tool{
+			Name:        "create-operator-template",
+			Description: "creates HW operator template",
+		},
+		tools.CreateOperatorTemplate,
+	)
 
 	// Run the server over stdin/stdout, until the client disconnects.
 	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
--- server/tools/create-operator-template.go
+++ server/tools/create-operator-template.go
@@ -1,9 +1,11 @@
 package tools
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
@@ -17,7 +19,46 @@ type Output struct {
 	DirName string `json:"dirName" jsonschema:"the directory containing the new operator code base"`
 }
 
+func runShellCommand(dir, command string, args []string) (string, string, error) {
+
+	var stdout, stderr bytes.Buffer
+
+	cmd := exec.Command(command, args...)
+	cmd.Dir = dir
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		return "", "", fmt.Errorf("failed to run command %v: %v", cmd, err)
+	}
+
+	return stdout.String(), stderr.String(), nil
+}
+
+func buildTemplaterIfNeeded() (string, string, error) {
+
+	templaterPath := "gpu-operator-templater/templater"
+	if _, err := os.Stat(templaterPath); os.IsNotExist(err) {
+		return runShellCommand("gpu-operator-templater", "make", []string{"templater"})
+	}
+
+	return "", "", nil
+}
+
 func CreateOperatorTemplate(ctx context.Context, req *mcp.CallToolRequest, input Input) (*mcp.CallToolResult, Output, error) {
+
+	stdout, stderr, err := buildTemplaterIfNeeded()
+	if err != nil {
+		result := &mcp.CallToolResult{
+			IsError: true,
+			Content: []mcp.Content{
+				&mcp.TextContent{
+					Text: fmt.Sprintf("Failed to build templater: %v\nStdout: %s\nStderr: %s", err, stdout, stderr),
+				},
+			},
+		}
+		return result, Output{}, fmt.Errorf("failed to build tempalter: %v", err)
+	}
+
 	operatorPath := filepath.Join("generated-operators", input.OperatorName)
 	if err := os.MkdirAll(operatorPath, 0755); err != nil {
 		return nil, Output{}, fmt.Errorf("couldn't create %s directory for the new operator: %v", operatorPath, err)
